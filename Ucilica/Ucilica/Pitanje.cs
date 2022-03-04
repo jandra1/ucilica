6401a5d34cf2a5a5ad92c4bdd13792fb7e1efa6c
marcihel05 <helena.marcius05@gmail.com>
2022-03-04 20:00:15 +0100

    database i klasa pitanje

--- Ucilica/Ucilica/Pitanje.cs
+++ Ucilica/Ucilica/Pitanje.cs
@@ -12,9 +12,16 @@ namespace Ucilica
 {
     public partial class Pitanje: Form
     {
+        string predmet;
+        int razred;
+        dataBase db = new dataBase();
+        Random rnd = new Random();
         public Pitanje()
         {
             InitializeComponent();
+            List<pitanjeKlasa> pitanja = db.getQuestionsByYearAndubject(razred, predmet);
+            int i = rnd.Next(0, pitanja.Count);
+            pitanjeTextBox.Text = pitanja[].pitanje;
         }
     }
 }
---
+++ Ucilica/Ucilica/pitanjeKlasa.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ucilica
+{
+    public class pitanjeKlasa
+    {
+        public int id;
+        public string pitanje;
+        public List<string> odgovori;
+        public string točan;
+        public string slika;
+        public string predmet;
+        public int razred;
+
+        public pitanjeKlasa() { }
+        public pitanjeKlasa(int id, string q, List<string> a, string c, string img)
+        {
+            pitanje = q;
+            odgovori = a;
+            točan = c;
+            slika = img;
+        }
+
+    }
+}
