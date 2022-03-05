2b21d86d98776dcd8780ccb64c3f1f4a1a7b063a
marcihel05 <helena.marcius05@gmail.com>
2022-03-05 12:43:35 +0100

    veći font

--- Ucilica/Ucilica/Pitanje.Designer.cs
+++ Ucilica/Ucilica/Pitanje.Designer.cs
@@ -36,11 +36,13 @@ namespace Ucilica
             this.odgovor3 = new System.Windows.Forms.Button();
             this.odgovor4 = new System.Windows.Forms.Button();
             this.time = new System.Windows.Forms.Label();
+            this.bodoviLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // pitanjeTextBox
             // 
             this.pitanjeTextBox.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.pitanjeTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
             this.pitanjeTextBox.Location = new System.Drawing.Point(39, 76);
             this.pitanjeTextBox.Multiline = true;
             this.pitanjeTextBox.Name = "pitanjeTextBox";
@@ -52,14 +54,16 @@ namespace Ucilica
             // 
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
             this.label1.Location = new System.Drawing.Point(36, 47);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(54, 16);
+            this.label1.Size = new System.Drawing.Size(82, 25);
             this.label1.TabIndex = 1;
             this.label1.Text = "Pitanje: ";
             // 
             // odgovor1
             // 
+            this.odgovor1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.odgovor1.Location = new System.Drawing.Point(144, 175);
             this.odgovor1.Name = "odgovor1";
             this.odgovor1.Size = new System.Drawing.Size(572, 60);
@@ -70,6 +74,7 @@ namespace Ucilica
             // 
             // odgovor2
             // 
+            this.odgovor2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.odgovor2.Location = new System.Drawing.Point(144, 253);
             this.odgovor2.Name = "odgovor2";
             this.odgovor2.Size = new System.Drawing.Size(572, 60);
@@ -80,6 +85,7 @@ namespace Ucilica
             // 
             // odgovor3
             // 
+            this.odgovor3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.odgovor3.Location = new System.Drawing.Point(144, 331);
             this.odgovor3.Name = "odgovor3";
             this.odgovor3.Size = new System.Drawing.Size(572, 60);
@@ -90,6 +96,7 @@ namespace Ucilica
             // 
             // odgovor4
             // 
+            this.odgovor4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.odgovor4.Location = new System.Drawing.Point(144, 412);
             this.odgovor4.Name = "odgovor4";
             this.odgovor4.Size = new System.Drawing.Size(572, 60);
@@ -101,11 +108,22 @@ namespace Ucilica
             // time
             // 
             this.time.AutoSize = true;
+            this.time.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
             this.time.Location = new System.Drawing.Point(910, 27);
             this.time.Name = "time";
-            this.time.Size = new System.Drawing.Size(44, 16);
+            this.time.Size = new System.Drawing.Size(53, 20);
             this.time.TabIndex = 6;
             this.time.Text = "label2";
+            // 
+            // bodoviLabel
+            // 
+            this.bodoviLabel.AutoSize = true;
+            this.bodoviLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.bodoviLabel.Location = new System.Drawing.Point(742, 412);
+            this.bodoviLabel.Name = "bodoviLabel";
+            this.bodoviLabel.Size = new System.Drawing.Size(53, 20);
+            this.bodoviLabel.TabIndex = 7;
+            this.bodoviLabel.Text = "label2";
             // 
             // Pitanje
             // 
@@ -113,6 +131,7 @@ namespace Ucilica
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Ucilica.Properties.Resources.ucionica;
             this.ClientSize = new System.Drawing.Size(1046, 487);
+            this.Controls.Add(this.bodoviLabel);
             this.Controls.Add(this.time);
             this.Controls.Add(this.odgovor4);
             this.Controls.Add(this.odgovor3);
@@ -136,5 +155,6 @@ namespace Ucilica
         private System.Windows.Forms.Button odgovor3;
         private System.Windows.Forms.Button odgovor4;
         private System.Windows.Forms.Label time;
+        private System.Windows.Forms.Label bodoviLabel;
     }
 }
